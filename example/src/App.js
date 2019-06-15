e2650178a5883375e1b5132002bacad128bb9cf9
rallets <stellar@rallets.com>
2019-06-15 18:26:26 +0800

    add 终局提示 终局判断

--- example/src/App.js
+++ example/src/App.js
@@ -1,34 +1,46 @@
-/* eslint-disable */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Chess from 'chess.js'
-import Chessground from 'react-chessground'
-import 'react-chessground/dist/styles/chessground.css'
-import { Statistic } from 'antd'
+/*eslint-disable*/
+import React from "react"
+import ReactDOM from "react-dom"
+import Chess from "chess.js"
+import Chessground from "react-chessground"
+import "react-chessground/dist/styles/chessground.css"
+import { Modal, Button } from "antd"
 
-const Countdown = Statistic.Countdown
-const deadline = Date.now()+1000*20
+const confirm = Modal.confirm
+const info = () => {
+  message.info("Check")
+}
 
-function onfinish() {
-  alert("Times up,you lose")
+const sleep = milliseconds => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+function isfinish() {
+  alert("You lose")
+}
+function ischeck() {
+  alert("Check")
 }
 
 class Demo extends React.Component {
   chess = new Chess()
 
   state = {
-    fen: '',
-    lastMove: null
+    visible: false,
+    visible2: false,
+    fen: "",
+    lastMove: null,
+    score1: 10,
+    score2: 10
   }
 
   turnColor() {
-    return (this.chess.turn() === 'w') ? 'white' : 'black'
+    return this.chess.turn() === "w" ? "white" : "black"
   }
 
   calcMovable() {
     const dests = {}
     this.chess.SQUARES.forEach(s => {
-      const ms = this.chess.moves({square: s, verbose: true})
+      const ms = this.chess.moves({ square: s, verbose: true })
       if (ms.length) dests[s] = ms.map(m => m.to)
     })
     return {
@@ -37,15 +49,26 @@ class Demo extends React.Component {
       color: this.myColor()
     }
   }
+
   myColor() {
-    return 'white'
+    return "white"
   }
 
   onMove = (from, to) => {
     const chess = this.chess
-    if (chess.move({ from, to, promotion: 'q' })) {
+    const moves = chess.moves({ verbose: true })
+    const move = moves[Math.floor(Math.random() * moves.length)]
+
+    if (chess.move({ from, to, promotion: "q" })) {
       this.setState({ fen: chess.fen(), lastMove: [from, to] })
+
       setTimeout(this.randomMove, 500)
+      if (this.chess.game_over() === true) {
+        this.setState({ 
+          visible: true,
+          score1: this.state.score1+10 })
+      }
+ console.log(this.state.score1)
     }
   }
 
@@ -56,33 +79,69 @@ class Demo extends React.Component {
     if (moves.length > 0) {
       chess.move(move.san)
       this.setState({ fen: chess.fen(), lastMove: [move.from, move.to] })
+      if (this.chess.game_over() === true) {
+        this.setState({ visible2: true,
+        score2:this.state.score2+10})
+      }
     }
+    sleep(1500).then(() => {
+      this.setState({ visible: false })
+    })
   }
-  reset = () =>{
+
+  reset = () => {
     this.chess.reset()
-    this.setState({fen: this.chess.fen()})
+    this.setState({ fen: this.chess.fen()})
   }
-  undo = () =>{
+
+  undo = () => {
     this.chess.undo()
-    this.setState({fen: this.chess.fen()})
+    this.setState({ fen: this.chess.fen() })
   }
+
+  showConfirm = () => {
+    const chess = this.chess
+    confirm({
+      title: "Do you Want to delete these items?",
+      content: "Some descriptions",
+      onOk() {
+        ;() => this.setState({ fen: chess.fen() })
+      },
+      onCancel() {
+        console.log("Cancel")
+      }
+    })
+  }
+
   render() {
-    return <div><Chessground
-        width={512}
-        height={512}
-        orientation={this.myColor()}
-        turnColor={this.turnColor()}
-        movable={this.calcMovable()}
-        lastMove={this.state.lastMove}
-        fen={this.state.fen}
-        onMove={this.onMove}
-        style={{ margin: 'auto' }}
-      />
-      <button onClick={() => this.reset() }>reset</button>
-      <button onClick={() => this.undo() }>undo</button>
-      <Countdown title="Timeleft" value={deadline} onFinish={onfinish} />
-    </div>
+    return (
+      <div>
+        <Chessground
+          width={512}
+          height={512}
+          orientation={this.myColor()}
+          turnColor={this.turnColor()}
+          movable={this.calcMovable()}
+          lastMove={this.state.lastMove}
+          score1={this.state.score1}
+          score2={this.state.score2}
+          fen={this.state.fen}
+          onMove={this.onMove}
+          style={{ margin: "auto" }}
+        />
+            <div>Your score:{this.state.score1}</div>
+        <Button onClick={() => this.reset()}>Reset</Button>
+        <Button onClick={() => this.showConfirm()}>Resign</Button>
+        <Button onClick={() => this.undo()}>Undo</Button>
+        <Modal visible={this.state.visible} footer={null}>
+          <p>Game over,you win</p>
+        </Modal>
+        <Modal visible={this.state.visible2} footer={null}>
+          <p>Game over,computer wins</p>
+        </Modal>
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<Demo />, document.getElementById('root'))
+ReactDOM.render(<Demo />, document.getElementById("root"))
