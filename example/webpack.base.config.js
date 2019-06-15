605ed4768d1230683a576bff1280411d2aa578a0
fyb <fyb@paiyou.co>
2019-06-15 15:39:18 +0800

    Revert "rm unused folders and codes in example"
    This reverts commit 5a34dc6df3d6d2f4799118e9f023f2cd8f692bad.

---
+++ example/webpack.base.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.join(__dirname, "/dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              exclude: /node_modules/,
+              ignore: false,
+              emitError: true,
+              useEslintrc: true
+            },
+            loader: require.resolve("eslint-loader")
+          }
+        ],
+        exclude: [/[/\\\\]node_modules[/\\\\]/, /html/]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: [
+                ["@babel/plugin-proposal-class-properties"],
+                ["import", { libraryName: "antd", style: "css" }]
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
---
+++ example/webpack.dev.config.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config.js');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 3000
+  }
+});
+
