3dcace9d4ad8bcdacbd789d982bd85a6bed1f8b7
hophacker <j@paiyou.co>
2020-01-19 13:02:47 +0800

    update all modules to latest, remove unsed modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,59 @@
-const gulp = require('gulp')
-const babel = require('gulp-babel')
-const uglifycss = require('gulp-uglifycss')
-const concat = require('gulp-concat')
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
 
 function getPresets(env) {
   return [
-    [ 'env', {
-      loose: true,
-      modules: env === 'es' ? false : 'commonjs'
-    } ],
-    'stage-1',
-    'react'
-  ]
+    [
+      "@babel/env",
+      {
+        loose: true,
+        modules: env === "es" ? false : "commonjs"
+      }
+    ],
+    "@babel/react"
+  ];
 }
 
-gulp.task('cjs', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(babel({
-      presets: getPresets('cjs')
-    }))
-    .pipe(gulp.dest('.'));
-})
+function getPlugins() {
+  return ["@babel/plugin-proposal-class-properties"];
+}
 
-gulp.task('es', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(babel({
-      presets: getPresets('es')
-    }))
-    .pipe(gulp.dest('es'));
-})
+gulp.task("cjs", () => {
+  return gulp
+    .src("src/**/*.js")
+    .pipe(
+      babel({
+        presets: getPresets("cjs"),
+        plugins: getPlugins()
+      })
+    )
+    .pipe(gulp.dest("."));
+});
 
-gulp.task('css', () => {
-  return gulp.src('src/styles/**/*.css')
+gulp.task("es", () => {
+  return gulp
+    .src("src/**/*.js")
+    .pipe(
+      babel({
+        presets: getPresets("es"),
+        plugins: getPlugins()
+      })
+    )
+    .pipe(gulp.dest("es"));
+});
+
+gulp.task("css", () => {
+  return gulp
+    .src("src/styles/**/*.css")
     .pipe(uglifycss())
-    .pipe(concat('chessground.css'))
-    .pipe(gulp.dest('dist/styles'))
-})
+    .pipe(concat("chessground.css"))
+    .pipe(gulp.dest("dist/styles"));
+});
 
-gulp.task('img', () => {
-  return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('dist/images'))
-})
+gulp.task("img", () => {
+  return gulp.src("src/images/**/*").pipe(gulp.dest("dist/images"));
+});
 
-gulp.task('default', ['cjs', 'es', 'css', 'img'])
-
+gulp.task("default", gulp.parallel(["cjs", "es", "css", "img"]));
