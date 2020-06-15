285b5b6b60988de76b514a830c5133cf85b6558a
fyb <fyb@paiyou.co>
2020-06-15 15:50:10 +0800

    example with gatsby

---
+++ example/with-gatsby/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Chessground from "react-chessground"
+import "react-chessground/dist/styles/chessground.css"
+
+const IndexPage = () => <Chessground />
+
+export default IndexPage
