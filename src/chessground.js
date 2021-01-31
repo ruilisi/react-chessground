c0cf72a39086ae48cf77d74bd759eb2e152a08e5
aaronb97 <aaron9becker7@gmail.com>
2021-01-30 20:09:01 -0500

    try setting auto shapes in render

--- src/chessground.js
+++ src/chessground.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Chessground as NativeChessground } from 'chessground'
+import React from "react";
+import PropTypes from "prop-types";
+import { Chessground as NativeChessground } from "chessground";
 
 export default class Chessground extends React.Component {
-
   static propTypes = {
     width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
@@ -31,54 +30,52 @@ export default class Chessground extends React.Component {
     onDropNewPiece: PropTypes.func,
     onSelect: PropTypes.func,
     items: PropTypes.object,
-    drawable: PropTypes.object
-  }
+    drawable: PropTypes.object,
+  };
 
   static defaultProps = {
     coordinates: true,
     resizable: true,
     highlight: {
       lastMove: true,
-      check: true
-    }
-  }
+      check: true,
+    },
+  };
 
   buildConfigFromProps(props) {
-    const config = { events: {} }
-    Object.keys(Chessground.propTypes).forEach(k => {
-      const v = props[k]
-      if (typeof v !== 'undefined') {
-        const match = k.match(/^on([A-Z]\S*)/)
+    const config = { events: {} };
+    Object.keys(Chessground.propTypes).forEach((k) => {
+      const v = props[k];
+      if (typeof v !== "undefined") {
+        const match = k.match(/^on([A-Z]\S*)/);
         if (match) {
-          config.events[match[1].toLowerCase()] = v
+          config.events[match[1].toLowerCase()] = v;
         } else {
-          config[k] = v
+          config[k] = v;
         }
       }
-    })
-    return config
-  }
-
-  componentDidMount() {
-    this.cg = NativeChessground(this.el, this.buildConfigFromProps(this.props))
+    });
+    return config;
   }
 
   componentWillReceiveProps(nextProps) {
-    this.cg.set(this.buildConfigFromProps(nextProps))
+    this.cg.set(this.buildConfigFromProps(nextProps));
   }
 
   componentWillUnmount() {
-    this.cg.destroy()
+    this.cg.destroy();
   }
 
   render() {
-    const props = { style: { ...this.props.style } }
+    this.cg.setAutoShapes(this.props.drawable.autoShapes);
+
+    const props = { style: { ...this.props.style } };
     if (this.props.width) {
-      props.style.width = this.props.width
+      props.style.width = this.props.width;
     }
     if (this.props.height) {
-      props.style.height = this.props.height
+      props.style.height = this.props.height;
     }
-    return <div ref={el => this.el = el} {...props} />
+    return <div ref={(el) => (this.el = el)} {...props} />;
   }
 }
